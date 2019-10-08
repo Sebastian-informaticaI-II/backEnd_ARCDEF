92f594dfe3be40f210bc9b96e532604366e3e127
sebastian <sebastian.insignares@udea.edu.co>
2019-10-07 23:48:51 -0500

    Versionado del ejecutable

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    if(argc == 1){
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+    std::string argumento(argv[1]);
+    if(argumento == "-V"){
+        std::cout << "La version del backEnd es: " << APP_VERSION << std::endl;
+    }else{
+        std::cout << "ERROR argumento desconocido. " << std::endl;
+    }
+    return 1;
 }
