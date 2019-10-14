502025e49cc79a88b8ca7cb20e9fb04858282e84
sebastian <sebastian.insignares@udea.edu.co>
2019-10-14 16:21:55 -0500

    Agregados sonido y fondo a mapa

--- MapaMar.cpp
+++ MapaMar.cpp
@@ -1,6 +1,25 @@
 #include "MapaMar.h"
 
-MapaMar::MapaMar()
+MapaMar::MapaMar(QWidget *parent)
 {
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600);
+    setBackgroundBrush(QBrush(QImage(":/images/fondo_claro")));
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+    //------------ Agregar elementos a escena ------------
 
+
+    //------------- Agregar Sonido de fondo --------------
+    QMediaPlaylist *playlist = new QMediaPlaylist();
+    playlist->addMedia(QUrl("qrc:/sounds/bgsound"));
+    playlist->setPlaybackMode(QMediaPlaylist::Loop);
+
+    QMediaPlayer *music = new QMediaPlayer();
+    music->setPlaylist(playlist);
+    music->play();
+    //---------------------- Mostrat ---------------------
+    show();
 }
